f1f434debd38e23c0c4bb27364a9f68c23b84d43
Noor Ahmed <noor.ahmed.biswas@framgia.com>
2015-11-06 09:51:44 +0600

    44499_Static_pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,22 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def pricing
+  end
+
+  def faq
+  end
+
+  def blog
+  end
+
+  def translators
+  end
+
+  def privacy_policy
+  end
+
+  def contact
+  end
+end
