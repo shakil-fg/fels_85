975565d4ee6fc80183276579939eea35fc063ed8
Noor Ahmed <noor.ahmed.biswas@framgia.com>
2015-11-10 10:42:58 +0600

    44246_login_logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,12 +11,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      log_in @user
+      flash[:success] = (t :welcome)
       redirect_to @user
     else
       render 'new'
     end
   end
-
 
   private
 
