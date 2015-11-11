4f5fc046a69269c7af52aa8ff88a09abb545f1bc
Tahmid Shakil <md.ahnaf.tahmid.shakil@framgia.com>
2015-11-11 18:46:15 +0600

    Wordlist View

--- app/models/word_answer.rb
+++ app/models/word_answer.rb
@@ -3,4 +3,7 @@ class WordAnswer < ActiveRecord::Base
   has_and_belongs_to_many :lessons
 
   validates :content,  presence: true
+
+  scope :correct, -> { where(correct: true) }
+  scope :correct_content, -> { correct.first.content }
 end
